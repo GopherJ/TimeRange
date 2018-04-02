19ea862ab181ca3011b835b1343cd4ad4ad8621c
Cheng JIANG <cheng.jiang@outlook.fr>
2018-04-02 22:41:25 +0200

    add time range

---
+++ src/main.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+import TimeRange from './components/TimeRange/TimeRange';
+import TimeRangeStore from './store/TimeRange';
+
+const install = (Vue, options = {}) => {
+    if (options.store) {
+        options.store.registerModule('TimeRange', TimeRangeStore);
+    }
+
+    // Vue.use(AnimatedVue);
+    Vue.component(TimeRange.name, TimeRange);
+};
+
+export default {
+    install,
+
+    TimeRange,
+    TimeRangeStore,
+};
+
